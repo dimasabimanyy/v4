e8c95dae57809388a83934d4e874079c8263b6c3
dimassabimanyy <dmsabmny@gmail.com>
2020-10-25 10:29:59 +0700

    add navigation menu style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,23 @@ import React from "react"
 import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
+import { Link } from 'gatsby'
 const Navbar = ({ toggleSidebar }) => {
   return <nav className="navbar">
-    <div className="nav-center">
+    {/* <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="logo"/>
         <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignRight></FaAlignRight>
         </button>
+      </div>
+      <PageLinks styleClass="nav-links"></PageLinks>
+    </div> */}
+    <div className="nav-center">
+      <div className="logo">
+        <Link to="/">
+          D'Abimanyu
+        </Link>
       </div>
       <PageLinks styleClass="nav-links"></PageLinks>
     </div>
