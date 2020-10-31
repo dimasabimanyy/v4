ad1a56d0eedccd86b16060895021c8089cdd94c0
dimassabimanyy <dmsabmny@gmail.com>
2020-10-31 13:00:37 +0700

    fix typewriter effect use typewriterJS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
-import Typical from 'react-typical'
+// import Typical from 'react-typical'
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {  
 
@@ -8,19 +9,15 @@ const Hero = () => {
   <section className="hero" id="hero">
     <div className="hero-wrapper" data-aos="fade-right" data-aos-duration="700" data-aos-delay="1350">
       <h1><span className="underlined underlined--offset--hero">Dimas Abimanyu</span></h1>
-      {/* <h4>Software Engineer</h4> */}
-      {/* <Typical
-        loop={Infinity}
-        wrapper="h4"
-        steps={[
-          'Full Stack Developer 💻',
-          1700,
-          'Open Sourcer 🔍',
-          1700,
-          'Content Creator 📹',
-          1700
-        ]}
-      /> */}
+      <h4>
+      <Typewriter
+        options={{
+          strings: ['Full Stack Developer 💻', 'Open Sourcer 🔍', 'Content Creator 📹'],
+          autoStart: true,
+          loop: true
+        }}
+      />
+      </h4>
       <SocialLinks styleClass="social-hero"></SocialLinks>
     </div>
   </section>
