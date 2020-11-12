90a895be9b827797145030c3440a8ba750a47761
dimassabimanyy <dmsabmny@gmail.com>
2020-11-12 20:45:19 +0700

    tweak hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,33 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect"
 
-const Hero = () => {  
-
-  return ( 
-  <section className="hero" id="hero">
-    <div className="hero-wrapper" data-aos="fade-right" data-aos-duration="700" data-aos-delay="1350">
-      <h1><span className="underlined underlined--offset--hero">Dimas Abimanyu</span></h1>
-      <h4>
-      <Typewriter
-        options={{
-          strings: ['Full Stack Developer 💻', 'Freelancer' ,'Open Sourcer 🔍', 'Content Creator 📹'],
-          autoStart: true,
-          loop: true
-        }}
-      />
-      </h4>
-      <SocialLinks styleClass="social-hero"></SocialLinks>
-    </div>
-  </section>
+const Hero = () => {
+  return (
+    <section className="hero" id="hero">
+      <div
+        className="hero-wrapper"
+        data-aos="fade-right"
+        data-aos-duration="700"
+        data-aos-delay="1350"
+      >
+        <h1>
+          <span className="underlined underlined--offset--hero">
+            Dimas Abimanyu
+          </span>
+        </h1>
+        <h4>
+          <Typewriter
+            options={{
+              strings: ["Full Stack Developer 💻", "Freelancer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h4>
+        <SocialLinks styleClass="social-hero"></SocialLinks>
+      </div>
+    </section>
   )
 }
 
